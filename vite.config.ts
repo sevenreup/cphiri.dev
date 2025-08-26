03da55910e89b2e23ea1305b75025ebf9c7eefa4
Christopher Seven Phiri <chris7phiri@gmail.com>
2025-08-26 10:36:25 +0200

    update

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,11 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
 import { imagetools } from '@zerodevx/svelte-img/vite';
 import tailwindcss from '@tailwindcss/vite';
+import devtoolsJson from 'vite-plugin-devtools-json';
 
 export default defineConfig({
 	ssr: { external: ['@resvg/resvg-js'] },
-	plugins: [sveltekit(), imagetools(), tailwindcss()],
+	plugins: [devtoolsJson(), sveltekit(), imagetools(), tailwindcss()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	},
